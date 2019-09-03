0d0fcae0bb3f32786dcc43aefa76f14b5f63079e
Shawna Summers <42246001+smsummers1@users.noreply.github.com>
2019-09-03 16:55:31 -0400

    Create 6_phpConstants.php

---
+++ 6_phpConstants.php
@@ -0,0 +1,17 @@
+    
+<!doctype html> 
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Constants</title>
+    </head>
+    
+    <body>
+      <?php #phpConstants.php
+      // Set today's date as a constant:
+      define('TODAY', 'April 23, 2022');
+      // Print a message, using predefined constants and the TODAY constant:
+      echo '<p>Today is ' . TODAY . '.<br>This server is running version <strong>' . PHP_VERSION . '</strong> of PHP on the <strong>' . PHP_OS . '</strong> operating system.</p>';
+      ?>
+    </body>
+</html>
