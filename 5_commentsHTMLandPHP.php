69913a73fe1c989a88909c430ddde37a311ff204
Shawna Summers <42246001+smsummers1@users.noreply.github.com>
2019-09-03 16:41:11 -0400

    Create 5_commentsHTMLandPHP.php

---
+++ 5_commentsHTMLandPHP.php
@@ -0,0 +1,31 @@
+<!doctype html> 
+<html lang="en">
+  <!-- This is a HTML comment
+       It is also multi-lined and the html code below will not render in the browser inside the comment tags.
+       <p> Why am I not working??? </p>
+       Comments are a great way to debug your code.
+       Comments do not render in a browser when you view source, but you can view them in an editor.
+   -->
+   
+  <head>
+    <meta charset="utf-8">
+    <title>Comments</title>
+  </head>
+  
+  <body>
+    <?php
+        # 5_commentsHTMLandPHP.php
+        # Created by:   Your name.....
+        # Description:  This is where you explain what your php script is supposed to be doing.
+
+        echo '<p>This is a line of text.<br>This is another line of text.</p>';
+
+        /*  This is a multi-line comment.
+            echo 'This line will not be executed.';
+        */
+
+        echo "<p>Now I'm done.</p>"; // End of PHP code.
+    ?>
+  </body>
+  
+</html>
