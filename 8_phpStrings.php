2240306a80cc0b3461e8e542a9844cb46a345462
Shawna Summers <42246001+smsummers1@users.noreply.github.com>
2019-09-04 23:11:05 -0400

    Create 8_phpStrings.php

---
+++ 8_phpStrings.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Strings</title>
+  </head>
+  <body>
+    <?php
+    
+      // Create string variables:
+      $first_name = 'Rasmus';
+      $last_name = 'Lerdorf';
+      $website = 'https://www.php.net/manual';
+      
+      // Print the values:
+      echo "<p>The php language was created by $first_name $last_name and all updates to php can be found at <a>$website</a>.</p>";
+      
+    ?>
+  </body>
+</html>
