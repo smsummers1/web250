ed4de671c1bb8c4d26606ef3487963b0184f1d72
Shawna Summers <42246001+smsummers1@users.noreply.github.com>
2019-09-04 23:18:49 -0400

    Create 11_Concatenation.php

---
+++ 11_Concatenation.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Concatenation</title>
+  </head>
+  
+  <body>
+    <?php
+    
+    // Create the variables:
+    $first_name = 'Julie';
+    $last_name = 'Jones';
+    $author = $first_name . ' ' . $last_name;
+    $book = 'The Girls\' Guide to Hunting and Fishing';
+    
+    //Print the values:
+    echo "<p>The book <em>$book</em> was written by $author.</p>";
+  ?>
+  </body>
+</html>
