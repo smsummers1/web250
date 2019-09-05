43dc055583bb55bdd46f35e71ad7caa2b3363f57
Shawna Summers <42246001+smsummers1@users.noreply.github.com>
2019-09-04 22:55:57 -0400

    Create 4_HTMLinPHPtagsEcho.php

---
+++ 4_HTMLinPHPtagsEcho.php
@@ -0,0 +1,13 @@
+
+<!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>HTML in PHP tags using echo</title>
+    </head>
+    <body>
+      <?php
+        echo '<i>Hello</i>, <b>world</b>!';
+      ?>
+    </body>
+  </html>
