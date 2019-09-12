58f40666c507832a9fe1eb7258cb7d00faed3fc0
Shawna Summers <42246001+smsummers1@users.noreply.github.com>
2019-09-11 23:23:02 -0400

    Create 3_handle_form.php

---
+++ Ch2/3_handle_form.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Form Feedback</title>
+  </head>
+  
+  <body>
+    <?php # Script 2.3 - handle_form.php
+      // Create variables to store the form data via $_REQUEST[]:
+      $name = $_REQUEST['name'];
+      $email = $_REQUEST['email'];
+      $comments = $_REQUEST['comments'];
+      
+      /* Not used:
+      $_REQUEST['age']
+      $_REQUEST['gender']
+      $_REQUEST['submit']
+      */
+      
+      // Print the submitted information:
+      echo "<p>Thank you, <strong>$name</strong>, for the following comments:</p>
+      <pre>$comments</pre><p>We will reply to you at <em>$email</em>.</p>\n";
+    ?>
+  </body>
+  
+</html>
